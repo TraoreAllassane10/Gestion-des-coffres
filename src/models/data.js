8c9e965eb55b1b39372aa526e9a73ffed6365106
Traore Allassane <traoreallassane2255@gmail.com>
2025-09-30 23:35:51 +0000

    Setup + Config

---
+++ src/models/data.js
@@ -0,0 +1,45 @@
+
+const coffres = [
+    {
+        id : 1,
+        nom : "Business",
+        montant : 0,
+        image : "business-case.png"
+    },
+     {
+        id : 2,
+        nom : "Etudes",
+        montant : 0,
+        image : "bourse-detudes.png"
+    },
+     {
+        id : 3,
+        nom : "Facture",
+        montant : 0,
+        image : "facture-dachat.png"
+    },
+     {
+        id : 4,
+        nom : "Fête",
+        montant : 0,
+        image : "fete.png"
+    },
+     {
+        id : 5,
+        nom : "Tontine",
+        montant : 0,
+        image : "plan-depargne.png"
+    },
+     {
+        id : 6,
+        nom : "Voyage",
+        montant : 0,
+        images : "sac-de-voyage.png"
+    },
+     {
+        id : 7,
+        nom : "Santé",
+        montant : 0,
+        image : "sante.png"
+    },
+];
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='max-w-2xl mx-auto py-16'>Home</div>
+  )
+}
+
+export default Home
